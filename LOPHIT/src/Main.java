d12ce4cc4c49ec140ec4e470c1544d52223fcafc
HITJAVA <leluong3107@gmail.com>
2025-10-14 19:46:51 +0700

    123

--- LOPHIT/src/Main.java
+++ LOPHIT/src/Main.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        int n;
+        Scanner sc = new Scanner(System.in);
+        System.out.print(" Moi nhap n: ");
+        n = sc.nextInt();
+        if(n <= 3600 && n > 0){
+            int a = n / 60 ;
+            int b = n % 60;
+            System.out.println("" + a + " phut va " + b + " giay");
+        }
+        else if (n <= 86400 && n > 3600){
+            int c = n / 3600;
+            int d = n % 3600;
+            int e = d / 60;
+            System.out.println("" + c + " tieng va " + e + " phut va " + d + "giay");
+        }
     }
 }
